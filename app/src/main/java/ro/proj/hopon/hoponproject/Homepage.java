7a56e397cba0dafe6ae5c781c94aa9dfe4c29e66
Brinza Alin Georgian <brinza.alin.georgian@gmail.com>
2015-11-19 01:26:24 +0200

    Start the Homepage activity by pressing the Log in button

--- app/src/main/java/ro/proj/hopon/hoponproject/Homepage.java
+++ app/src/main/java/ro/proj/hopon/hoponproject/Homepage.java
@@ -1,41 +1,22 @@
 package ro.proj.hopon.hoponproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-
-public class Login extends AppCompatActivity {
-
-    private static Button button_login;
+public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_frame);
-
-
-        button_login = (Button) findViewById(R.id.login_BTN);
-        button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Homepage.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.homepage_frame);
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login_frame, menu);
+        getMenuInflater().inflate(R.menu.menu_homepage_frame, menu);
         return true;
     }
 
